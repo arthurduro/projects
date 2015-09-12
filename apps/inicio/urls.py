23f897141e45e8a179fc2d971614e5044822f998
jose daniel <josedanielf9@gmail.com>
2015-09-12 13:30:20 -0400

    login configurado

--- apps/inicio/urls.py
+++ apps/inicio/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 
-from .views import Index, LoginView, LogoutView
+from .views import Index, LoginView, LogoutView, Inicio
 
 
 urlpatterns = [
 	url(r'^$', Index.as_view(), name='index'),
+	url(r'^inicio/$', Inicio.as_view(), name='inicio'),
    	url(r'^login/$', LoginView.as_view(), name='login'),
   	url(r'^logout/$', LogoutView.as_view(), name='logout'),
   	
--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, FormView, RedirectView
+from django.views.generic import TemplateView, FormView, RedirectView, View
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 
 # Create your views here.
-class Index(TemplateView):
+class Inicio(TemplateView):
 	template_name= "inicio/index.html"
+
+
+    
+class Index(View):
+
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect(reverse_lazy('login'))
+        else:
+            return HttpResponseRedirect(reverse_lazy('inicio'))     
+
 
 class LoginView(FormView):
     form_class = AuthenticationForm
